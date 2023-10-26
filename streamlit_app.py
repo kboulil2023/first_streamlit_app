ecc6231236c346221cbd583dad50f9882d785229
kboulil2023 <148458331+kboulil2023@users.noreply.github.com>
2023-10-26 21:08:57 +0200

    Update streamlit_app.py
    Allow a user to add a fruit to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,16 +45,15 @@ streamlit.dataframe(fruityvice_normalized)
 # Change the Streamlit Components to Make Things Look a Little Nicer
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * from fruit_load_list")
-#Let's Get All the Rows, Not Just One
-#my_data_row = my_cur.fetchone()
 my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text("The fruit load list contains:")
 streamlit.header("The fruit load list contains:")
-#streamlit.text(my_data_row)
 streamlit.dataframe(my_data_rows)
+
+# Allow a user to add a fruit to the list : lab
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'lemon')
+streamlit.write('Thanks for adding', add_my_fruit)
+
 
 
 
